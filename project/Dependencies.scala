3008a35d97350b611b98e0a6bfbf088589ee5b12
Fernando Brûlé <fernandobrule@gmail.com>
2022-04-04 15:48:38 -0300

    update deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val fragnosticConfEnv          = "com.fragnostic"        %  "fragnostic-conf-env_2.13"      % "0.1.9-SNAPSHOT"
+  lazy val fragnosticConfEnv          = "com.fragnostic"        %  "fragnostic-conf-env_2.13"      % "0.1.9"
 
   lazy val logbackClassic             = "ch.qos.logback"        % "logback-classic"                % "1.3.0-alpha12" % "runtime"
   lazy val slf4jApi                   = "org.slf4j"             % "slf4j-api"                      % "2.0.0-alpha5"
